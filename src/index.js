dc23ffb33426f01c32011e342eec8f91bed7abbd
Miroslav Hrachovec <52509976+mhrachovec@users.noreply.github.com>
2022-06-07 21:28:55 +0200

    Fixed implementation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 (async () => {
-  import { h, app } from 'https://unpkg.com/hyperapp@2.0.22/index.js?module'
+  const { h, text, app } = await import('https://unpkg.com/hyperapp@2.0.22')
+  const useText = txt => ('string', 'number', 'bignumber').includes(typeof txt) ? text(txt) : txt
+  const html = new Proxy({}, {
+    get: (target, name) => name === 'text' ? text : (attrs = {}, child) => !child ? h(name, {}, useText(attrs)) : h(name, attrs, useText(child))
+  })
 
+  const { main, h1, button } = html
   app({
     init: () => 0,
     view: state =>
-      h('div', {}, [
-        h('h1', {}, state),
-        h('button', { onclick: state => state - 1 }, '-'),
-        h('button', { onclick: state => state + 1 }, '+')
+      main([
+        h1(state),
+        button({ onclick: state => state - 1 }, text('-')),
+        button({ onclick: state => state + 1 }, text('+'))
       ]),
     node: document.getElementById('app')
   })
-})();
+})()
