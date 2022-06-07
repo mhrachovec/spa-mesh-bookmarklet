b1824969e8fe6c748982164b738025306fef6d78
Miroslav Hrachovec <52509976+mhrachovec@users.noreply.github.com>
2022-06-07 21:55:29 +0200

    Move implementation to module

---
+++ src/app/index.js
@@ -0,0 +1,19 @@
+import { h, text, app } from 'https://unpkg.com/hyperapp@2.0.22/index.js'
+
+const useText = txt => ('string', 'number', 'bignumber').includes(typeof txt) ? text(txt) : txt
+const html = new Proxy({}, {
+  get: (target, name) => name === 'text' ? text : (attrs = {}, child) => !child ? h(name, {}, useText(attrs)) : h(name, attrs, useText(child))
+})
+
+const { main, h1, button } = html
+
+export default ({ node }) => app({
+  init: () => 0,
+  view: state =>
+    main([
+      h1(state),
+      button({ onclick: state => state - 1 }, text('-')),
+      button({ onclick: state => state + 1 }, text('+'))
+    ]),
+  node
+})
