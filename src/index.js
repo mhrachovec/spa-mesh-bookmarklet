e73cb26d858625774ba01502598b7d517a3e4c6b
Miroslav Hrachovec <52509976+mhrachovec@users.noreply.github.com>
2022-06-07 17:26:53 +0200

    Simple counter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 (async () => {
-  document.getElementById('app').innerHTML = '<h1>Works!</h1>'
+  const { h, app } = import('https://unpkg.com/hyperapp@2.0.22/index.js?module')
+
+  app({
+    init: () => 0,
+    view: state =>
+      h('div', {}, [
+        h('h1', {}, state),
+        h('button', { onclick: state => state - 1 }, '-'),
+        h('button', { onclick: state => state + 1 }, '+')
+      ]),
+    node: document.getElementById('app')
+  })
 })();
